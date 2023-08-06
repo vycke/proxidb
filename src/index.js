3e01acc156f79910da0b69afba5e9cbec480f306
Kevin Pennekamp <kevin@crinkles.dev>
2023-08-06 16:00:23 +0200

    fix: updated internals for cleaner code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
+// returns a promise wrapper around the IndexedDB store code through a proxy
+function storeGetHandler(store, method) {
+  return async function (...args) {
+    return new Promise((resolve, reject) => {
+      if (!store[method]) reject("Method not supported");
+      const request = store[method](...args);
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = () => reject(request.error);
+    });
+  };
+}
+
+// a proxy wrapper that allows direct access to 'stores' or functions of an
+// opened database connection
 function dbGetHandler(db, key) {
   // first tries to get the store. if store does not exist
   // for key, invoke the normal db[key]
   try {
     const store = db.transaction(key, "readwrite").objectStore(key);
-
-    function get(_, method) {
-      return async function (...args) {
-        return new Promise((resolve, reject) => {
-          if (!store[method]) reject("Method not supported");
-          const request = store[method](...args);
-          request.onsuccess = () => resolve(request.result);
-          request.onerror = () => reject(request.error);
-        });
-      };
-    }
-
-    return new Proxy({}, { get });
+    return new Proxy(store, { get: storeGetHandler });
   } catch (e) {
     return db[key];
   }
@@ -30,7 +32,7 @@ export async function idb(name, version, upgrade) {
       const db = connection.result;
       resolve(new Proxy(db, { get: dbGetHandler }));
     };
-    // Return the errors
+
     connection.onerror = (event) => {
       reject(event.target.errorCode);
     };
